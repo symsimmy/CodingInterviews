e6a08686e3e79ea6a6dffa54b36336f1f484b952
symsimmy <symsimmys@gamil.com>
2018-09-04 18:28:44 +0800

    014,数值的整数次方，未完成

---
+++ src/main/java/SwordOffer/P014Power.java
@@ -0,0 +1,15 @@
+package SwordOffer;
+
+public class P014Power {
+    public static void main(String[] args) {
+        P014Power main = new P014Power();
+        double base = 2.4;
+        int exponent = -1;
+        System.out.println(main.Power(base,exponent));
+    }
+
+    public int Power(double base,int exponent){
+        return 0;
+    }
+
+}
